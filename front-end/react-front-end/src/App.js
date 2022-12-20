34d9f56f8babb62b8789bd60390fc99f28417203
Joakim <superapa.spanx@gmail.com>
2022-12-20 19:57:48 +0100

    commit

--- front-end/react-front-end/src/App.js
+++ front-end/react-front-end/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
 
 function App() {
+
+  console.log("Hello!");
+
+  const reqBody={
+    "username" :"joakim",
+    "password" :1234
+  }
+
+  fetch("api/auth/login",{
+    headers: {
+        "Content-Type": "application/json"
+    },
+    method:"post",
+    body: JSON.stringify(reqBody)
+  })
+    .then((response) => Promise.all([response.json(), response.headers]))
+    .then(([body,headers]) =>
+      {const authValue = headers.get("Authorization");
+      console.log(authValue);
+      console.log(body);
+
+    });
+
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
     </div>
-  );
+  ); 
 }
 
 export default App;
