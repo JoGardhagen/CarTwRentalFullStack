22e231e844677ad3797cc3d780a831a32803179f
Joakim <superapa.spanx@gmail.com>
2022-12-23 21:12:55 +0100

    NewStuff

--- front-end/react-front-end/src/Dashboard/index.js
+++ front-end/react-front-end/src/Dashboard/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useLocalState } from "../util/useLocalStorage";
 
 const Dashboard = () => {
 
     const [jwt,setJwt] = useLocalState("", "jwt");
+
+    const [assignments,setAssignments] = useState(null);
+
+    useEffect(() =>{
+        fetch("api/assignments",{
+            headers:{
+                "Content-Type" : "application/json",
+                "Authorization": `Bearer ${jwt}`
+            },
+            method: "GET",
+        }).then(response => {
+            if(response.status === 200)return response.json();
+        }).then((assignmentsData) => {
+            setAssignments(assignmentsData);
+        });
+    },[]);
 
     function createAssignment(){
         fetch("/api/assignments",{
@@ -24,7 +41,12 @@ const Dashboard = () => {
         <div style={{margin:"2em"}}>
             {/* <h1>Hello World!</h1>
             <div>JWT value is {jwt}</div> */}
-
+            {assignments ? assignments.map((assignments) => (
+            <div><Link to = {`/assignment/${assignments.id}`}>
+                Assignment ID: {assignments.id}</Link></div>
+             )) : (
+             <></>
+             )}
             <button onClick={()=> createAssignment()}>Submit new Assignment</button>
         </div>
         
