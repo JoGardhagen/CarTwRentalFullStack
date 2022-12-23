c735cc24ae76de92c1df402b5273d2a224a0f217
Joakim <superapa.spanx@gmail.com>
2022-12-23 17:32:56 +0100

    new

--- front-end/react-front-end/src/Dashboard/index.js
+++ front-end/react-front-end/src/Dashboard/index.js
@@ -5,10 +5,27 @@ const Dashboard = () => {
 
     const [jwt,setJwt] = useLocalState("", "jwt");
 
+    function createAssignment(){
+        fetch("/api/assignments",{
+            headers:{
+                "content-type" : "application/json",
+                Authorization : `Bearer ${jwt}`,
+            },
+            method:"POST",
+        }).then(response =>{
+            if(response.status === 200) return response.json();
+        })
+        .then((assignment)=>{
+            window.location.href = `/assignments/${assignment.id}`;
+        });
+    }
+
     return (
-        <div>
-            <h1>Hello World!</h1>
-            <div>JWT value is {jwt}</div>
+        <div style={{margin:"2em"}}>
+            {/* <h1>Hello World!</h1>
+            <div>JWT value is {jwt}</div> */}
+
+            <button onClick={()=> createAssignment()}>Submit new Assignment</button>
         </div>
         
     );
